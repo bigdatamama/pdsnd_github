c0c822ae0c054422dfc9d99413b3360f689dc89b
Mary Beth Backus <mbbackus69@gmail.com>
2024-09-12 17:45:38 -0400

    Change 1 to bikeshare.py for Udacity project (I hate Python)

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks the user to specify a city, month, and day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
@@ -24,7 +24,7 @@ def get_filters():
         print("That is not an option. Please try again.") #handle errors in input
         city = input("Which city would you like to review: Chicago, New York City, or Washington? ").strip().lower() #make case insensitive      
 
-    # TO DO: get user input for month (all, january, february, ... , june)
+    # TO DO: get user input for month (all, january, february, ... , june) I really hate python. It is not intuitive AT ALL.
     valid_months = ["all", "january", "february", "march", "april", "may", "june"] #define valid months
     month = input("Which month would you like to review: all, january, february, march, april, may, or june? ").strip().lower() #make case insensitive
 
